a6bf92f86b6dfdb9afe9ba5ed3e1478490d30ca8
avatheavenger <andrewyurenja@gmail.com>
2015-11-18 08:35:29 +0200

    ResultPanel script

--- Assets/Scripts/UI/ResultPanel.cs
+++ Assets/Scripts/UI/ResultPanel.cs
@@ -27,51 +27,11 @@ public class ResultPanel : MonoBehaviour
 
     void OnEnable() {
         if (LevelManager.Instance == null || GameManager.Instance == null || GameManager.Instance.Player == null) { return; }
-        int currentExp = LevelManager.Instance.GetExp();
-        int currentLevel = LevelManager.Instance.GetLevel();
-        int expEarned = GameManager.Instance.Player.Exp;
-        int[] levelsExp = LevelManager.Instance.levelsExp;
-        int overallLevelExp = 0;
-        for (int i = 0; i < currentLevel + 1; i++) {
-            overallLevelExp += levelsExp[i];
-        }
-        expSlider.value = (currentExp - overallLevelExp) / (float)levelsExp[currentLevel + 1];
-        for (int i = currentLevel; i < levelsExp.Length - 1; i++) {
-            if (currentExp + expEarned > levelsExp[i + 1]) {
-                levelsAcquired++;
-            }
-            else {
-                break;
-            }
-        }
-        if (levelsAcquired > 0) {
-            expEarned -= levelsExp[levelsAcquired] - currentExp;
-        }
-
-        for (int i = 0; i < currentLevel + 1 + levelsAcquired; i++) {
-            overallLevelExp += levelsExp[i];
-        }
-        int previousLevelExp = currentLevel > 0 ? levelsExp[currentLevel - 1] : 0;
-        if (currentLevel >= 0 && currentLevel < 9) {
-            targetValue = (currentExp + expEarned - overallLevelExp) / (float)(levelsExp[currentLevel + 1 + levelsAcquired] + previousLevelExp);
-            LevelManager.Instance.AddExp(GameManager.Instance.Player.Exp);
-            updateUI = true;
-        }
+        CalcLevelProgress();
     }
 
     void Update() {
         if (!updateUI) { return; }
-        if (levelsAcquired > 0) {
-            if (Mathf.Abs(expSlider.value - 1f) > fillPrecision) {
-                expSlider.value += (1.0f / (int)(1 / (fillWaitTime * 1f))) * Time.deltaTime;
-            }
-            else {
-                levelsAcquired--;
-                expSlider.value = 0.0f;
-                EventManager.Instance.FireLevelUp();
-            }
-            return;
-        }
         if (Mathf.Abs(expSlider.value - targetValue) > fillPrecision) {
             expSlider.value += (1.0f / (int)(1 / (fillWaitTime * targetValue))) * Time.deltaTime;
         }
@@ -81,6 +41,34 @@ public class ResultPanel : MonoBehaviour
                 EventManager.Instance.FireLevelUp();
                 expSlider.value = 0;
             }
+            if (overhead > 0) {
+                CalcLevelProgress();
+            }
+        }
+    }
+
+    private int overhead = 0;
+
+    private void CalcLevelProgress() {
+        int currentExp = LevelManager.Instance.GetExp();
+        int currentLevel = LevelManager.Instance.GetLevel();
+        int expEarned = overhead > 0 ? overhead : GameManager.Instance.Player.Exp;
+        int[] levelsExp = LevelManager.Instance.levelsExp;
+        if (true) { // change to current level < MAX_LEVEL
+            int expToNextLevel = levelsExp[currentLevel + 1];
+            if (overhead == 0) {
+                expSlider.value = currentExp / (float)expToNextLevel;
+            }
+            if (expEarned + currentExp > expToNextLevel) {
+                overhead = expEarned - (expToNextLevel - currentExp);
+            }
+            else {
+                overhead = 0;
+            }
+            int remainder = expEarned - overhead;
+            targetValue = (remainder + currentExp) / (float)expToNextLevel;
+            LevelManager.Instance.AddExp(remainder);
+            updateUI = true;
         }
     }
 }
