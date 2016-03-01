d86e7f2cc147a7ac2085322f2393468ca87482e4
avatheavenger <andrewyurenja@gmail.com>
2016-03-01 16:23:32 +0200

    refactored LM

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -24,50 +24,41 @@ public class LevelManager : MonoBehaviour
     public int[] strangeMachineTimers;
     public SkeletonDataAsset[] skins;
 
-	
-	public delegate void OnPurchase(object arg);
-	
-	public OnPurchase InGameMoneyPurchase;
 
-    public static LevelManager Instance
-    {
+    public delegate void OnPurchase(object arg);
+
+    public OnPurchase InGameMoneyPurchase;
+
+    public static LevelManager Instance {
         get { return instance; }
     }
 
-    void Awake()
-    {
-        if (instance == null)
-        {
+    void Awake() {
+        if (instance == null) {
             instance = this;
             DontDestroyOnLoad(this);
         }
-        else
-        {
+        else {
             DestroyImmediate(this);
         }
         List<Skin> ownedSkins = GetOwnedSkins();
-        if (!ownedSkins.Contains(Skin.NONE))
-        {
+        if (!ownedSkins.Contains(Skin.NONE)) {
             BuySkin(Skin.NONE);
         }
     }
 
-    public void AddExp(int amt)
-    {
+    public void AddExp(int amt) {
         PlayerPrefs.SetInt(EXP_KEY, GetExp() + amt);
     }
 
-    public int GetExp()
-    {
+    public int GetExp() {
         return PlayerPrefs.GetInt(EXP_KEY, 0);
     }
 
-    public int GetExpThisLevel()
-    {
+    public int GetExpThisLevel() {
         int overallExp = GetExp();
         int expForPrevLevels = 0;
-        for (int i = 0; i < GetLevel() + 1; i++)
-        {
+        for (int i = 0; i < GetLevel() + 1; i++) {
             expForPrevLevels += levelsExp[i];
         }
         return overallExp - expForPrevLevels;
@@ -77,62 +68,50 @@ public class LevelManager : MonoBehaviour
         return GetLevel() == 10;
     }
 
-    public int GetLevel()
-    {
+    public int GetLevel() {
         int currentExp = GetExp();
         int level = 0;
-        for (int i = 1; i < levelsExp.Length; i++)
-        {
-            if (currentExp >= levelsExp[i])
-            {
+        for (int i = 1; i < levelsExp.Length; i++) {
+            if (currentExp >= levelsExp[i]) {
                 level = i;
                 currentExp -= levelsExp[i];
             }
-            else
-            {
+            else {
                 break;
             }
         }
         return level;
     }
 
-    public void AddMoney(int amt)
-    {
+    public void AddMoney(int amt) {
         PlayerPrefs.SetInt(MONEY_KEY, GetMoney() + amt);
     }
 
-    public int GetMoney()
-    {
+    public int GetMoney() {
         return PlayerPrefs.GetInt(MONEY_KEY, 0);
     }
 
-    public void UpgradePowerUpLevel(PowerUp PowerUp)
-    {
+    public void UpgradePowerUpLevel(PowerUp PowerUp) {
         int currentPowerUpLevel = GetPowerUpLevel(PowerUp);
-        if (currentPowerUpLevel >= maxPowerUpLevel)
-        {
+        if (currentPowerUpLevel >= maxPowerUpLevel) {
             return;
         }
         PlayerPrefs.SetInt(PowerUp.ToString(), currentPowerUpLevel + 1);
     }
 
-    public int GetPowerUpLevel(PowerUp powerUp)
-    {
+    public int GetPowerUpLevel(PowerUp powerUp) {
         return PlayerPrefs.GetInt(powerUp.ToString(), 0);
     }
 
-    public void AquireConsumable(Consumable consumable)
-    {
+    public void AquireConsumable(Consumable consumable) {
         PlayerPrefs.SetInt(consumable.ToString(), 1);
     }
 
-    public bool IsConsumableActive(Consumable consumable)
-    {
+    public bool IsConsumableActive(Consumable consumable) {
         return PlayerPrefs.GetInt(consumable.ToString(), 0) == 1;
     }
 
-    public void DeactivateConsumable(Consumable consumable)
-    {
+    public void DeactivateConsumable(Consumable consumable) {
         PlayerPrefs.SetInt(consumable.ToString(), 0);
     }
 
@@ -148,80 +127,65 @@ public class LevelManager : MonoBehaviour
         return 0;
     }*/
 
-	public void AquireInGameMoney(MenuUI.Chest chestName) {
-		InGameMoneyPurchase (chestName);
-	}
+    public void AquireInGameMoney(MenuUI.Chest chestName) {
+        InGameMoneyPurchase(chestName);
+    }
 
-	public void AddMoneyFromChest(MenuUI.Chest chestName) {
-		switch (chestName) {
-			case MenuUI.Chest.SMALL: AddMoney(5500); break;
-			case MenuUI.Chest.MEDIUM: AddMoney(12000); break;
-			case MenuUI.Chest.BIG: AddMoney(31000); break;
-			case MenuUI.Chest.XXL: AddMoney(65000); break;
-		}
-	}
+    public void AddMoneyFromChest(MenuUI.Chest chestName) {
+        switch (chestName) {
+            case MenuUI.Chest.SMALL: AddMoney(5500); break;
+            case MenuUI.Chest.MEDIUM: AddMoney(12000); break;
+            case MenuUI.Chest.BIG: AddMoney(31000); break;
+            case MenuUI.Chest.XXL: AddMoney(65000); break;
+        }
+    }
 
-    public void ActivatePassiveSkill(Passive passive)
-    {
+    public void ActivatePassiveSkill(Passive passive) {
         PlayerPrefs.SetInt(passive.ToString(), 1);
-        foreach (Passive skill in Passive.GetValues(typeof(Passive)))
-        {
-            if (skill != passive)
-            {
+        foreach (Passive skill in Passive.GetValues(typeof(Passive))) {
+            if (skill != passive) {
                 PlayerPrefs.SetInt(skill.ToString(), 0);
             }
         }
     }
 
-    public List<Consumable> GetActiveConsumables()
-    {
+    public List<Consumable> GetActiveConsumables() {
         List<Consumable> activeConsumables = new List<Consumable>();
-        foreach (Consumable c in System.Enum.GetValues(typeof(Consumable)))
-        {
-            if (IsConsumableActive(c))
-            {
+        foreach (Consumable c in System.Enum.GetValues(typeof(Consumable))) {
+            if (IsConsumableActive(c)) {
                 activeConsumables.Add(c);
             }
         }
         return activeConsumables;
     }
 
-    public Passive GetActivePassive()
-    {
+    public Passive GetActivePassive() {
         Passive result = Passive.NONE;
-        foreach (Passive skill in Passive.GetValues(typeof(Passive)))
-        {
-            if (PlayerPrefs.GetInt(skill.ToString(), 0) == 1)
-            {
+        foreach (Passive skill in Passive.GetValues(typeof(Passive))) {
+            if (PlayerPrefs.GetInt(skill.ToString(), 0) == 1) {
                 result = skill;
             }
         }
         return result;
     }
     //LevelManager.Instance.BuySkin (Skin.NONE);
-    public void BuySkin(Skin skin)
-    {
+    public void BuySkin(Skin skin) {
         PlayerPrefs.SetInt(skin.ToString(), 1);
     }
 
     //LevelManager.Instance.HasSkin (Skin.NONE);
-    public bool HasSkin(Skin skin)
-    {
+    public bool HasSkin(Skin skin) {
         return PlayerPrefs.GetInt(skin.ToString(), 0) == 1;
     }
 
-    public bool HasSkin(int index)
-    {
+    public bool HasSkin(int index) {
         return HasSkin(GetSkinByIndex(index));
     }
 
-    public List<Skin> GetOwnedSkins()
-    {
+    public List<Skin> GetOwnedSkins() {
         List<Skin> skins = new List<Skin>();
-        foreach (Skin skin in Skin.GetValues(typeof(Skin)))
-        {
-            if (PlayerPrefs.GetInt(skin.ToString(), 0) == 1)
-            {
+        foreach (Skin skin in Skin.GetValues(typeof(Skin))) {
+            if (PlayerPrefs.GetInt(skin.ToString(), 0) == 1) {
                 skins.Add(skin);
             }
         }
@@ -229,73 +193,58 @@ public class LevelManager : MonoBehaviour
     }
 
     //LevelManager.Instance.EquipSkin (Skin.NONE);
-    public void EquipSkin(Skin skin)
-    {
-        if (!GetOwnedSkins().Contains(skin))
-        {
+    public void EquipSkin(Skin skin) {
+        if (!GetOwnedSkins().Contains(skin)) {
             return;
         }
 
         PlayerPrefs.SetString(ACTIVE_SKIN_KEY, skin.ToString());
     }
 
-    public void EquipSkin(int index)
-    {
+    public void EquipSkin(int index) {
         EquipSkin(GetSkinByIndex(index));
     }
 
-    private Skin GetSkinByIndex(int index)
-    {
+    private Skin GetSkinByIndex(int index) {
         return (Skin)System.Enum.GetValues(typeof(Skin)).GetValue(index);
     }
 
-    public Skin GetEquippedSkin()
-    {
-        foreach (Skin skin in GetOwnedSkins())
-        {
-            if (PlayerPrefs.GetString(ACTIVE_SKIN_KEY, Skin.NONE.ToString()).Equals(skin.ToString()))
-            {
+    public Skin GetEquippedSkin() {
+        foreach (Skin skin in GetOwnedSkins()) {
+            if (PlayerPrefs.GetString(ACTIVE_SKIN_KEY, Skin.NONE.ToString()).Equals(skin.ToString())) {
                 return skin;
             }
         }
         return Skin.NONE;
     }
     //Int to load directly special shop
-    public void SetShopID(int index)
-    {
+    public void SetShopID(int index) {
         PlayerPrefs.SetInt(SHOP_ID, GetShopID() + index);
     }
-    public int GetShopID()
-    {
+    public int GetShopID() {
         return PlayerPrefs.GetInt(SHOP_ID, 0);
     }
 
-    public void SaveAchievementProgress(Achievement achievement, int amt)
-    {
+    public void SaveAchievementProgress(Achievement achievement, int amt) {
         PlayerPrefs.SetInt(achievement.ToString(), amt);
     }
 
-    public int GetAchievementProgress(Achievement achievement)
-    {
+    public int GetAchievementProgress(Achievement achievement) {
         return PlayerPrefs.GetInt(achievement.ToString(), 0);
     }
 
-    public int GetBestScore()
-    {
+    public int GetBestScore() {
         return PlayerPrefs.GetInt(BEST_SCORE, 0);
     }
 
-    public void SaveBestScore(int score)
-    {
+    public void SaveBestScore(int score) {
         PlayerPrefs.SetInt(BEST_SCORE, score);
     }
     //Sound check
-    public void SetSoundCkeck(int index)
-    {
+    public void SetSoundCkeck(int index) {
         PlayerPrefs.SetInt(SOUND_CHECK, GetSoundCheck() + index);
     }
-    public int GetSoundCheck()
-    {
+    public int GetSoundCheck() {
         return PlayerPrefs.GetInt(SOUND_CHECK, 0);
     }
 
@@ -325,17 +274,17 @@ public class LevelManager : MonoBehaviour
         NONE
     }
 
-    public int GetItemIndex<T>(T item)
-    {
+    public int GetItemIndex<T>(T item) {
         System.Array itemArray = System.Enum.GetValues(typeof(T));
-        for (int i = 0; i < itemArray.Length; i++)
-        {
-            if (item.Equals(itemArray.GetValue(i)))
-            {
+        for (int i = 0; i < itemArray.Length; i++) {
+            if (item.Equals(itemArray.GetValue(i))) {
                 return i;
             }
         }
         return 0;
     }
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) { Application.Quit(); }
+    }
 }
