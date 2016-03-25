6f42b621f6fe465d9135c211b9b0666d4c770150
avatheavenger <andrewyurenja@gmail.com>
2016-03-25 15:46:24 +0200

    Missing Scripts

---
+++ Assets/Scripts/FlyingObstacle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Side = GameManager.Side;
+
+public class FlyingObstacle : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public float distanceToPlayer = 10.0f;
+
+	private Transform player;
+	private bool isWarningShown = false;
+	private Side side;
+
+	// Use this for initialization
+	void Start () {
+		SpawnableObject so = GetComponent<SpawnableObject>();
+		Vector3 pos = transform.position;
+		float rng = Random.Range(0, 1);
+		if (rng > 0.5f) {
+			pos.y = 2;
+			side = Side.UPPER;
+		} else {
+			pos.y = -7;
+			side = Side.BOTTOM;
+		}
+		transform.position = pos;
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		if (!isWarningShown) {
+			EventManager.FireObstacleWarning(true, side);
+			isWarningShown = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed * Vector2.left * Time.deltaTime);
+		if (shouldDisableWarning() && isWarningShown) {
+			EventManager.FireObstacleWarning(false, side);
+			isWarningShown = false;
+		}
+	}
+
+	private bool shouldDisableWarning() {
+		return transform.position.x - player.position.x < distanceToPlayer;
+	}
+}
---
+++ Assets/Scripts/UI/ObstacleWarning.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ObstacleWarning : MonoBehaviour {
+
+	private Image warningImg;
+
+	public GameManager.Side side;
+	// Use this for initialization
+	void Start () {
+		warningImg = GetComponent<Image>();
+		EventManager.OnObstacleWarning += (show, side) => {
+			if(!this.side.Equals(side)) { return; }
+			Color currentColor = warningImg.color;
+			if(!show) {
+				currentColor.a = 0;
+			} else {
+				currentColor.a = 255;
+			}
+			warningImg.color = currentColor;
+		};
+	}
+}
