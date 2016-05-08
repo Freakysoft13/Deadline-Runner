516d957d9ea8428b34a7a786b7806081b1df74af
avatheavenger <andrewyurenja@gmail.com>
2016-05-08 13:26:52 +0300

    Missing files

---
+++ Assets/Scripts/GooglePlayServices.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class GooglePlayServices : MonoBehaviour
+{
+
+    private bool isLoggedIn = false;
+
+
+    private static GooglePlayServices instance = null;
+
+    public static GooglePlayServices Instance
+    {
+        get { return instance; }
+    }
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            DestroyImmediate(this);
+        }
+    }
+
+    public void Login(Action callback)
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            isLoggedIn = success;
+            if (callback != null)
+            {
+                callback();
+            }
+        });
+    }
+
+    public void ShowAchievements()
+    {
+        if (isLoggedIn)
+        {
+            Social.ShowAchievementsUI();
+        }
+        else
+        {
+            Login(() =>
+            {
+                Social.ShowAchievementsUI();
+            });
+        }
+    }
+
+    public void ShowLeaderboard()
+    {
+        if (isLoggedIn)
+        {
+            Social.ShowLeaderboardUI();
+        }
+        else
+        {
+            Login(() =>
+            {
+                Social.ShowLeaderboardUI();
+            });
+        }
+    }
+
+    public void ReportScore(String leaderboard, long score)
+    {
+        Social.ReportScore(score, leaderboard, null);
+    }
+
+    public void ReportProgress(String achievementID, float progress)
+    {
+        Social.ReportProgress(achievementID, progress, null);
+    }
+}
