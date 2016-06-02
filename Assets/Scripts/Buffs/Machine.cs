ab018e200e74a81436ad689a368ca2aaeb91145e
avatheavenger <andrewyurenja@gmail.com>
2016-06-02 09:29:40 +0300

    Machine prefab

---
+++ Assets/Scripts/Buffs/Machine.cs
@@ -0,0 +1,15 @@
+﻿namespace Effect
+{
+    public class Machine : Effect
+    {
+        public override void ApplyEffect(Player player) {
+            player.ToggleCar(true);
+        }
+
+        public override void WearOff(Player player) {
+        }
+
+        public override void RemoveEffect(Player player) {
+        }
+    }
+}
