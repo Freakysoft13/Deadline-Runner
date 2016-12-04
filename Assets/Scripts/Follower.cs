29bf725cd969b63cca5c46c91fa32d2730a73c4f
avatheavenger <andrewyurenja@gmail.com>
2016-12-04 16:20:48 +0200

    BG on new camera
    BG on new camera

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Follower : MonoBehaviour {
+
+	public Transform trackedObject;
+    private Vector3 velocity = Vector3.zero;
+    private float xAhead = 0.0f;
+
+    void Start()
+    {
+        xAhead = trackedObject.position.x - transform.position.x;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 futurePos = transform.position;
+        futurePos.x = trackedObject.position.x - xAhead;
+        transform.position = Vector3.SmoothDamp(transform.position, futurePos, ref velocity, 0.04f);
+    }
+}
---
+++ Assets/Scripts/SimpleMover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SimpleMover : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+		transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
