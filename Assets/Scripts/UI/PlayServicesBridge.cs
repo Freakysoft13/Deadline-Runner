1bdf705426dbae7bd8320e163a02f827f017e965
avatheavenger <andrewyurenja@gmail.com>
2016-12-16 17:34:33 +0200

    Missing script

---
+++ Assets/Scripts/UI/PlayServicesBridge.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayServicesBridge : MonoBehaviour
+{
+
+    public void ShowLeaderBoard()
+    {
+        GooglePlayServices.Instance.ShowLadder();
+    }
+    public void ShowAchievements()
+    {
+        GooglePlayServices.Instance.ShowAchievements();
+    }
+}
